bcec55b4a22eeb21ab887343b3151eb41a52f7ce
costar011 <dnaer456@gmail.com>
2022-04-06 23:38:32 +0900

    116

---
+++ add2.c
@@ -0,0 +1,24 @@
+/* 쉽게 풀어쓴 C언어 express */
+
+/*
+    116P 
+
+    사용자로부터 입력받은 2개의 정수의 합을 계산하여 출력 
+
+*/
+
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void)
+{
+    int x;
+    int y;
+    int sum;
+
+    printf("첫 번쨰 숫자를 입력하시오.");
+
+  
+
+    return 0;
+} 
