db8af8c19e872c26b9c4ba88a3135033ea7ca199
costar011 <dnaer456@gmail.com>
2022-04-28 16:47:04 +0900

    146

---
+++ cstu1/two_compl.c
@@ -0,0 +1,16 @@
+/* 쉽게 풀어쓴 C언어 express 146 */
+
+#include <stdio.h>
+
+int main(void) {
+
+    int x = 3;
+    int y = -3;
+
+    printf("x = %08X\n",x);
+    printf("y = %08X\n",y);
+    printf("x+y = %08X\n",x+y);
+
+    return 0;
+
+}
