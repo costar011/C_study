893734cac3f7b8eef175b07cc67f983381012b36
costar011 <dnaer456@gmail.com>
2022-05-11 00:07:16 +0900

    159

---
+++ cstu1/escape.c
@@ -0,0 +1,17 @@
+/* 쉽게 풀어쓴 C언어 express 159 */
+
+
+#include <stdio.h>
+
+int main(main) {
+    int id, pass;
+
+    printf("");
+    printf("");
+    scanf("");
+    printf("");
+    scanf("");
+    printf("");
+    return 0;
+
+}
