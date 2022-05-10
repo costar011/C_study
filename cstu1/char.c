bb06a37ce10b8f8efa25cdc5770edae18f1e3e02
costar011 <dnaer456@gmail.com>
2022-05-10 23:55:06 +0900

    160

---
+++ cstu1/char.c
@@ -0,0 +1,15 @@
+/* 쉽게 풀어쓴 C언어 express 160 */
+
+
+#include <stdio.h>
+
+int main() {
+
+   char code = 'A'; 
+
+   printf("%d %d %d\n",code,code+1,code+2);
+   printf("%c %c %c\n", code, code+1, code+2);
+
+    return 0;
+
+}
