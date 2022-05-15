92451b653b777909d41d9047d17e4ecdc14effba
costar011 <dnaer456@gmail.com>
2022-05-15 22:32:50 +0900

    159

--- cstu1/escape.c
+++ cstu1/escape.c
@@ -6,12 +6,12 @@
 int main(main) {
     int id, pass;
 
-    printf("");
-    printf("");
-    scanf("");
-    printf("");
-    scanf("");
-    printf("");
+    printf("아이디와 패스워드를 4개의 숫ㅏ로 입력하시오.:\n");
+    printf("id ____\b\b\b\b");
+    scanf("%d",&id);
+    printf("pass ____ \b\b\b\b");
+    scanf("%d", &pass);
+    printf("\a 입력된 아이디는 \"%d \"이고 패스워드는 \"%d \"입니다\n", id, pass);
     return 0;
 
 }
