4b510a2dc7addd0d377ff829f250c32e300ea341
costar011 <dnaer456@gmail.com>
2022-05-27 00:18:35 +0900

    176

---
+++ cstu2/arithmetic1.c
@@ -0,0 +1,16 @@
+/* 쉽게 풀어쓴 C언어 express 175 */
+
+#include <stdio.h>
+
+int main(void) {
+
+    double x,y,result;
+
+    printf("두개의 실수를 입력하시오");
+    scanf("%lf %lf", &x, &y)
+    result = x + y;
+    printf("");
+
+    return 0;
+
+}
