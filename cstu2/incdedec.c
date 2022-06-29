e0985d1fe0274e0893ec7597340b06d3c14f0811
costar011 <dnaer456@gmail.com>
2022-06-29 23:20:07 +0900

    179

---
+++ cstu2/incdedec.c
@@ -0,0 +1,23 @@
+/* 쉽게 풀어쓴 C언어 express 179 */
+
+
+// 나머지 연산자 프로그램
+#include <stdio.h>
+
+
+int main(void) {
+
+    int x=10; y=10;
+
+    printf("x=%d\n", x);
+    printf("++x의 값=%d\n", ++X);
+    printf("x=%d\n\n", x);
+
+    printf("y=%d\n", y);
+    printf("y++의 값=%d\n", y++);
+    printf("y=%d\n", y);
+
+
+    return 0;
+
+}
