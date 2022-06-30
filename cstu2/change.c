1a32f74c345a9aac8ff17682145cee3901816ed9
costar011 <dnaer456@gmail.com>
2022-06-30 16:48:41 +0900

    180

---
+++ cstu2/change.c
@@ -0,0 +1,21 @@
+/* 쉽게 풀어쓴 C언어 express 180 */
+
+#include <stdio.h>
+
+int main(void) {
+
+    int user, change = 0;
+    int price, c5000, c1000, c500, c100;
+
+    printf("물건 값을 입력하시오.");
+    scanf("%d", &price);
+
+    printf("사용자가 낸 돈: ");
+    scanf("%d", &user);
+
+    change = uesr - price;
+
+    c5000 = change / 5000;
+    change = change % 5000;
+
+}
