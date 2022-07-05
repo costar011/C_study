46ad6b7591af9f8132914ffe54ef098792891548
costar011 <dnaer456@gmail.com>
2022-07-05 23:52:54 +0900

    180

--- cstu2/change.c
+++ cstu2/change.c
@@ -27,10 +27,10 @@ int main(void) {
     c100 = change / 100;
     change = change % 100;
 
-    printf("오천원권: ");
-    printf("");
-    printf("");
-    printf("");
+    printf("오천원권: %d장\n", c5000);
+    printf("천원권: %d장\n", c1000);
+    printf("오백원 동전: %d장\n", c500);
+    printf("백원 동전: %d장\n", c100);
     
     return 0;
 
