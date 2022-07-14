ea0213a85af5d9276ce279a652904d65a3a3d06b
costar011 <dnaer456@gmail.com>
2022-07-14 21:31:12 +0900

    183

--- cstu2/assignment.c
+++ cstu2/assignment.c
@@ -6,5 +6,13 @@ int main(void) {
 
     int x,y;
 
+    x = 1;
+
+    printf("수식 x+1 값은 %d\n", x+1);
+    printf("수식 y=x+1 값은 %d\n", y=x+1);
+    printf("수식 y=10+(x=2+7) 값은 %d\n", y=10+(x=2+7));
+    printf("수식 y=x=3 값은 %d\n", y=x=3);
+
+    return 0;
 
 }
