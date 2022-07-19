1e9f19f525f06f71ccef2a0f77a9ba55ad6b2476
costar011 <dnaer456@gmail.com>
2022-07-19 16:23:38 +0900

    186

---
+++ cstu2/relational.c
@@ -0,0 +1,23 @@
+/* 쉽게 풀어쓴 C언어 express 186 */
+
+#include <stdio.h>
+
+int main(void) {
+
+    int x,y;
+
+    printf("");
+    scanf("%d%d", &x, &y);
+
+
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    
+
+    return 0;
+
+}
