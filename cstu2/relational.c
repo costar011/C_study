27f66be5326223fe7b70b1c540dd4a0a793e6a51
costar011 <dnaer456@gmail.com>
2022-07-20 19:41:04 +0900

    186

--- cstu2/relational.c
+++ cstu2/relational.c
@@ -6,16 +6,16 @@ int main(void) {
 
     int x,y;
 
-    printf("");
+    printf("두개의 정수를 입력하시오 :");
     scanf("%d%d", &x, &y);
 
 
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
+    printf("x == y의 결과값: %d\n", x == y);
+    printf("x != y의 결과값: %d\n", x != y);
+    printf("x > y의 결과값: %d\n", x > y);
+    printf("x < y의 결과값: %d\n", x < y);
+    printf("x >= y의 결과값: %d\n", x >= y);
+    printf("x <= y의 결과값: %d\n", x <= y);
     
 
     return 0;
