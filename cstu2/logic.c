b89e79deb4203c2788545c36b77fbd337f94cdbe
costar011 <dnaer456@gmail.com>
2022-07-21 12:13:28 +0900

    191

---
+++ cstu2/logic.c
@@ -0,0 +1,22 @@
+/* 쉽게 풀어쓴 C언어 express 191 */
+
+
+#include <stdio.h>
+
+int main(void) {
+
+    int x,y;
+
+    printf("두개의 정수를 입력하시오 :");
+    scanf("%d%d", &x, &y);
+
+
+    printf("");
+    printf("");
+    printf("");
+
+   
+
+    return 0;
+
+}
