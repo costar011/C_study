cbc840b903406e8dd461964637f8e8492cdae2b6
costar011 <dnaer456@gmail.com>
2022-07-31 17:06:58 +0900

    204

---
+++ cstu2/convert1.c
@@ -0,0 +1,20 @@
+/* 쉽게 풀어쓴 C언어 express 204 */
+
+#include <stdio.h>
+
+int main(void) {
+
+char c;
+int i;
+float f;
+
+c = 1000;
+i =1.23456 + 10;
+f = 10 + 20;
+
+printf("c = %d, i = %d, f = %f\n", c, i , f);
+
+return 0;
+
+
+}   
