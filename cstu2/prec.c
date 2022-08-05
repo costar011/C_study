41ab8139f8925350cd53f91c7fef75cde4b8a986
costar011 <dnaer456@gmail.com>
2022-08-05 15:11:37 +0900

    213

---
+++ cstu2/prec.c
@@ -0,0 +1,27 @@
+/* 쉽게 풀어쓴 C언어 express 213 */
+
+#include <stdio.h>
+
+int main(void) {
+
+    int x=0, y=0;
+    int result;
+
+    result = 2 > 3 || 6  > 7 ;
+    printf("%d\n", result);
+
+    result = 2 || 3 && 3 >2 ;
+    printf("%d\n", result);
+
+
+    result = x = y = 1;
+    printf("%d\n", result);
+
+
+    result = - ++x + y--;
+    printf("%d\n", result);
+
+
+    return 0;
+
+}
