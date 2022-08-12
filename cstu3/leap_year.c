c3c6109dfc3b3b0be439ce5718570ab51391367b
costar011 <dnaer456@gmail.com>
2022-08-12 23:41:12 +0900

    236

---
+++ cstu3/leap_year.c
@@ -0,0 +1,19 @@
+/* 쉽게 풀어쓴 C언어 express 236 */
+
+#include <stdio.h>
+
+int main(void) {
+
+int year;
+
+printf("분자를 입력하세요: ");
+scanf("%d", &n);
+
+if( d == 0)
+    printf("0으로 나눌 수 없습니다.\n");
+else    
+    printf("결과는 %d입니다.\n", result);
+
+return 0;
+
+}
