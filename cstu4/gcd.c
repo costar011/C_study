1e59001b234ae2de064475f49c63df14098f5353
costar011 <dnaer456@gmail.com>
2022-09-03 09:20:30 +0900

    277

--- cstu4/gcd.c
+++ cstu4/gcd.c
@@ -8,7 +8,15 @@ int main(void) {
 int x, y, r;
 
 printf("두개의 정수를 입력하시오.");
+scanf("%d%d", &x, &y);
 
+while( y ! 0) 
+{
+    r = x % y;
+    x = y;
+    y = r;
+}
+printf("최대 공약수는 %d입니다.\n", x);
 
 return 0;
 
---
+++ cstu4/radio.c
@@ -0,0 +1,9 @@
+/* 쉽게 풀어쓴 C언어 express 278 */
+
+#include <stdio.h>
+
+int main(void) {
+
+return 0;
+
+}
