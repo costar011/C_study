d4f5e29800a4203e356ac142095131f817b99727
costar011 <dnaer456@gmail.com>
2022-09-15 00:35:31 +0900

    278

--- cstu4/radio.c
+++ cstu4/radio.c
@@ -1,2 +1,14 @@
 /* 쉽게 풀어쓴 C언어 express 278 */
 
+int main(void) {
+    int halflife;
+    double initial;
+    double current;
+    int years = 0;
+
+    printf("반감기를 입력하시오(년) : ");
+    scanf("%d", &halflife);
+
+    return 0;
+
+}
