a57493b4181e8f1a280187707874a7795446ae4a
costar011 <dnaer456@gmail.com>
2022-09-16 20:00:29 +0900

    278

--- cstu4/radio.c
+++ cstu4/radio.c
@@ -9,6 +9,17 @@ int main(void) {
     printf("반감기를 입력하시오(년) : ");
     scanf("%d", &halflife);
 
+    initial = 100.0;
+    current = initial;
+    while(current > initial/10.0)
+    {
+        years += halflife;
+        current = initial;
+        printf("%d년 후에 남은 양=%f\n", years, current);
+    }
+
+    printf("1/10 이하로 되기까지 걸린 시간=%d년\n", years);
+
     return 0;
 
 }
