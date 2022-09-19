6ec942610bc73e9ef0d895edadac57ec4312520b
costar011 <dnaer456@gmail.com>
2022-09-19 20:41:07 +0900

    280

--- study/while.c
+++ study/while.c
@@ -8,7 +8,13 @@ int main(void) {
 int number, sum = 0;
 
 // 루프 몸체가 적어도 한번은 실행한다.
+do {
+    printf("정수를 입력하시오 :");
+    scanf("%d", &number);
+    sum += number;
+} while (number != 0);
 
+printf("숫자들의 합 = %d\n", sum);
 
 return 0;
 
