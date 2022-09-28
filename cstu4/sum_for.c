75da4169a806b41ebcc6ea9fff420181e6daedd5
costar011 <dnaer456@gmail.com>
2022-09-29 00:30:41 +0900

    287

---
+++ cstu4/sum_for.c
@@ -0,0 +1,17 @@
+/* 쉽게 풀어쓴 C언어 express 287 */
+
+// 반복을 이용한 정수합 프로그램
+int main(void) {
+
+int sum;
+
+for(i = 1; i <= 10; i++) // i는 1부터 10까지 증가
+    sum += i;
+
+    printf("1부터 10까지 정수의 합 = %d\n", sum);
+
+
+
+return 0;
+
+}
