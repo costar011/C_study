d83e5fa2862d17745a324a525a70202056ff0a36
costar011 <dnaer456@gmail.com>
2022-10-12 19:15:01 +0900

    298

--- cstu4/square_root.c
+++ cstu4/square_root.c
@@ -1,11 +1,19 @@
 /* 쉽게 풀어쓴 C언어 express 298 */
 
 #include <stdio.h>
-#include <math.h>
+#define SEED_MONEY 1000000 
 
 int main(void) {
 
+while(1)
+{
+    year++;
+    money += money * 0.30;
+    if(money > 10 * SEED_MONEY)
+        break;
+}
 
+printf("%d년\n", year);
 
 return 0;
 
