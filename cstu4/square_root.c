e74d5980281d3ceadf7bba58e3a1fd4d9083b7fb
costar011 <dnaer456@gmail.com>
2022-10-13 13:08:39 +0900

    298

--- cstu4/square_root.c
+++ cstu4/square_root.c
@@ -1,19 +1,22 @@
 /* 쉽게 풀어쓴 C언어 express 298 */
 
 #include <stdio.h>
-#define SEED_MONEY 1000000 
+#include <math.h>
 
 int main(void) {
 
+double v;
+
 while(1)
 {
-    year++;
-    money += money * 0.30;
-    if(money > 10 * SEED_MONEY)
-        break;
-}
+    printf("실수값을 입력하시오");
+    scanf("%lf", &v);
 
-printf("%d년\n", year);
+    if(v < 0.0)
+        break;
+
+    printf("%f의 제곱근은 %f입니다.\n", v, sqrt(v));
+}
 
 return 0;
 
