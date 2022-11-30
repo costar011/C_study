3de0f4dfb4cb0e4df17f20f1c6ffb7dea0e47ff7
costar011 <dnaer456@gmail.com>
2022-11-30 23:57:08 +0900

    307

--- cstu5/gambler.c
+++ cstu5/gambler.c
@@ -13,10 +13,19 @@ int main(void) {
 
     for(i = 0; i < 100; i++) {
         int cash = initial_money;
-        while (cash)
+        while (cash > 0 && cash < goal) {
+            if(((double)rand() / RAND_MAX) < 0.5) cash++;
+            else cash --;
+        }
+
+        if(cash == goal) wins++;
     }
 
-return 0;
+    printf("초기 금액 $%d\n", initial_money);
+    printf("목표 금액 $%d\n", goal);
+    printf("100번 중에서 %d번 성공\n", wins);
+
+    return 0;
 
 }
  
