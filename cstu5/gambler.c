dd36e08ed8904fb13fadb386b83e725d019bb36e
costar011 <dnaer456@gmail.com>
2022-11-30 23:50:24 +0900

    307

---
+++ cstu5/gambler.c
@@ -0,0 +1,22 @@
+/* 쉽게 풀어쓴 C언어 express 307*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+
+    int initial_money = 50;
+    int goal = 250;
+    int i;
+    int wins = 0;
+
+    for(i = 0; i < 100; i++) {
+        int cash = initial_money;
+        while (cash)
+    }
+
+return 0;
+
+}
+ 
